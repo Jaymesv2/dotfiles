1fad3d68dd436877bc934c7fe9597994166582d7
Trent <ghastfilms613@gmail.com>
2024-05-19 19:19:04 -0500

    updated flake

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-gaming.url = "github:fufexan/nix-gaming";
+    # nix-gaming.inputs.nixpkgs.flollows = "nixpkgs";
   };
+
   outputs = inputs: with inputs; {
     defaultPackage = home-manager.defaultPackage;
+
     homeConfigurations = {
       trent = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
@@ -16,21 +20,32 @@
 	      config.permittedInsecurePackages = [
             "electron-25.9.0"
           ];
-	    };
-	    extraSpecialArgs = { 
-          pkgs-unstable = import nixpkgs {
-	        system = "x86_64-linux";
-	        config.allowUnfree = true;
-	      };
-	    };
-	    modules = [ ./home.nix ];
+	};
+	extraSpecialArgs = {
+	  nix-gaming = inputs.nix-gaming;
+          pkgs-unstable = import nixpkgs-unstable {
+	    system = "x86_64-linux";
+	    config.allowUnfree = true;
+	  };
+        };
+        modules = [ ./home.nix ];
       };
     };
+
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	    modules = [ ./system/configuration.nix ];
+        specialArgs = {
+          pkgs-unstable = import nixpkgs-unstable {
+            system = "x86_64-linux";
+          };
+          nixpkgs = nixpkgs;
+          nixpkgs-unstable = nixpkgs-unstable;
+          nixpkgs2311 = nixpkgs;
+        };
       };
     };
+
   };
 }
