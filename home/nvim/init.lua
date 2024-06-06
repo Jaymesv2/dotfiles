0fd89dd03c983cd115cad72e7a7134f6851f26dd
Trent <ghastfilms613@gmail.com>
2024-06-06 13:27:19 -0500

    update nvim conf

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -1,1 +1,37 @@
 require('options')
+
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
+
+require('keymaps')
+
+require('dashboard').setup {
+      -- config
+}
+
+require('Comment').setup()
+
+require('overseer').setup()
+
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { { "prettierd", "prettier" } },
+  },
+})
+
+require('gitsigns').setup()
+
+local ls = require('luasnip')
+vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
--- home/nvim/lua/keymaps.lua
+++ home/nvim/lua/keymaps.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ','
+
 -- define common options
 local opts = {
     noremap = true,      -- non-recursive
@@ -19,8 +22,28 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 -- delta: 2 lines
 vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize -2<CR>', opts)
+
+-- reset 
+vim.keymap.set('n', '<Leader>h', ':noh<CR>')
+-- map <leader>o :setlocal spell! spelllang=en_us<CR>
+-- map <leader>n :NERDTreeToggle<CR>
+vim.keymap.set('n', '<Leader>n', function() vim.cmd('NvimTreeToggle')  end)
+
+vim.keymap.set('n', '<Leader>y', '"+y')
+vim.keymap.set('n', '<Leader>p', '"+p')
+vim.keymap.set('n', '<Leader>Y', '"*y')
+vim.keymap.set('n', '<Leader>P', '"*p')
+
+vim.keymap.set('n', '<Leader>ff', function() require('telescope.builtin').find_files() end)
+vim.keymap.set('n', '<Leader>fg', function() require('telescope.builtin').live_grep() end )
+vim.keymap.set('n', '<Leader>fb', function() require('telescope.builtin').buffers() end)
+vim.keymap.set('n', '<Leader>fh', function() require('telescope.builtin').help_tags() end )
+vim.keymap.set('n', '<Leader>fc', function() require('telescope.builtin').tags() end)
+
+
+
 
 -----------------
 -- Visual mode --
@@ -29,4 +52,10 @@ vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 -- Hint: start visual mode with the same area as the previous area and the same mode
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
+
+
+vim.keymap.set("n", "<leader>bp", function()
+    require('cokeline.mappings').pick("focus")
+end, { desc = "Pick a buffer to focus" })
+
 
