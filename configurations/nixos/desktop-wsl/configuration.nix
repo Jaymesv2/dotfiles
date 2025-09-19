2e4a8517e1bb8ac521edca1c85072356aa6522db
Trent <ghastfilms613@gmail.com>
2025-09-18 20:51:04 -0500

    updated nixos config for wsl

--- configurations/nixos/desktop-wsl/configuration.nix
+++ configurations/nixos/desktop-wsl/configuration.nix
@@ -7,5 +7,40 @@
     networking.hostName = "desktop-wsl";
 
 
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.trent = {
+      isNormalUser = true;
+      description = "Trent";
+      group = "trent";
+      shell = pkgs.zsh;
+      extraGroups = [ "wheel" ];
+      # packages = with pkgs; [];
+      # initialPassword = "123abc"; # best password
+    };
+
+
+    programs.zsh.enable = true;
+    environment.pathsToLink = [ "/share/zsh" ];
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+      # (nerdfonts.override { } )
+    ];
+
+
     system.stateVersion = "25.05";
 }
