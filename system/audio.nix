ced99b8366e8af4578fd54a95fc4be143ffe9b53
Trent <ghastfilms613@gmail.com>
2026-01-24 18:49:28 -0600

    enabled jack

--- system/audio.nix
+++ system/audio.nix
@@ -13,15 +13,14 @@
         };
     };
 
-    hardware.pulseaudio.enable = false;
-
     services.blueman.enable = true;
-
+    services.pulseaudio.enable = false;
     services.pipewire  = {
         enable = true;
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
+        jack.enable = true;
         # If you want to use JACK applications, uncomment this
         #jack.enable = true;
         # wireplumber.extraConfig.bluetoothEnhancements = {
