b8556c5e2928d1df4f6bd8abaa1dfbc6cf3e667a
Trent <ghastfilms613@gmail.com>
2026-01-27 12:03:16 -0600

    added unused hibernation config

--- system/hardware/laptop/hibernate.nix
+++ system/hardware/laptop/hibernate.nix
@@ -1,13 +1,23 @@
 {
-  # boot = {
-  #   kernelParams = [
-  #       "resume_offset=533760"
-  #   ];
-  #   resumeDevice = "/dev/disk/by-label/nixos";
-  # };
-  # boot.initrd = {
-  #   supportedFilesystems = [ "btrfs" "vfat" ];
-  #   kernelModules = [ "btrfs" "dm-mod" "dm-crypt" ];
-  #   availableKernelModules = [ "btrfs" "dm-mod" "dm-crypt" ];
-  # };
+  boot = {
+    kernelParams = [
+        "resume_offset=533760"
+    ];
+    resumeDevice = "/dev/disk/by-label/nixos";
+  };
+
+  # Hibernate on power button pressed
+  services.logind.settings.Login.LidSwitch = "suspend-then-hibernate";
+  services.logind.settings.Login.PowerKey = "hibernate";
+  services.logind.settings.Login.PowerKeyLongPress = "poweroff";
+
+  # Suspend first
+  boot.kernelParams = ["mem_sleep_default=deep"];
+
+  # Define time delay for hibernation
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=2h
+    HibernateOnACPower=no
+    SuspendState=mem
+  '';
 }
