d914a5494213d194aa297b8b0d328622ce5c1a87
Trent <ghastfilms613@gmail.com>
2026-02-04 13:39:50 -0600

    removed AC power target and switched to upower along with other updates

--- system/laptop.nix
+++ system/laptop.nix
@@ -1,49 +1,17 @@
 { pkgs, lib, config, options, ...} : {
     #imports = [./hardware/new_laptop.nix];
-
     imports = [ ./hardware/laptop/disk.nix ./hardware/laptop/hardware-configuration.nix ./hardware/laptop/secureboot.nix ];
     # fileSystems."/var/log".neededForBoot = true;
-    
-    # boot.kernelParams = [ "amdgpu.abmlevel=0" "mt7925e.disable_aspm=1"];
 
+    boot.initrd.systemd.network.wait-online.enable = false;
+    systemd.network.wait-online.enable = false;
+    boot.loader.timeout = 1;
 
-    # boot.extraModprobeConfig = ''
-    #   options mt7925e disable_aspm=1
-    # '';
-    # networking.wireless.driver = "mt7921e";
+    boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-zen4;
+    #linuxPackages-cachyos-latest;
+    # linux-cachyos-latest-lto-zen4
+    # linux-cachyos-bore-lto
 
-
-    # ac-power is enabled when the system is connected to ac power :|
-    systemd.targets.ac-power = {
-        enable = true;
-        description = "AC-only services";
-    };
-    
-    systemd.services.toggle-ac-power = {
-        description = "toggles between performance and balanced depending on power";
-        
-        partOf = ["ac-power.target"];
-        wantedBy=["ac-power.target"];
-
-        serviceConfig = {
-            Type = "oneshot";
-            RemainAfterExit="yes";
-            ExecStart = ''${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance'';
-            ExecStop = ''${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced'';
-        };
-    };
-    
-    services.udev.extraRules = lib.mkAfter ''
-        SUBSYSTEM=="power_supply", KERNEL=="AC*", ATTRS{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl start ac-power.target"
-        SUBSYSTEM=="power_supply", KERNEL=="AC*", ATTRS{online}=="0", RUN+="${pkgs.systemd}/bin/systemctl stop ac-power.target"
-    '';
-
-    # services.udev.extraRules = lib.mkAfter ''
-    #     SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0012", ATTR{power/wakeup}="disabled", ATTR{driver/1-1.1.1.4/power/wakeup}="disabled"
-    #     SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0014", ATTR{power/wakeup}="disabled", ATTR{driver/1-1.1.1.4/power/wakeup}="disabled"
-    # '';
-    
-    
     services.acpid.enable = true;
     # detect when the system is connected to ac power 
     powerManagement.enable = true;
@@ -53,16 +21,8 @@
     services.thermald.enable = true;
 
     services.fprintd.enable = true;
+    services.upower.enable = true;
 
-    
-
-    # systemd.sleep.extraConfig = ''
-    #     [Sleep]
-    #     AllowSuspendThenHibernate=yes
-    #     HibernateDelaySec=2h
-    # '';
-
-    # Nvidia Configuration 
     services.xserver.videoDrivers = [ "amdgpu" ]; 
 
     hardware.graphics = {
