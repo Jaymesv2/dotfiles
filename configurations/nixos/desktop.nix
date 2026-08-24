37c299fe1503d11c837cd96f2a9853d81a48082b
Trent <ghastfilms613@gmail.com>
2026-08-24 12:58:05 -0500

    added groups to desktop config and updated flake lock

--- configurations/nixos/desktop.nix
+++ configurations/nixos/desktop.nix
@@ -122,7 +122,7 @@
     description = "Trent";
     group = "trent";
     shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "tss" config.users.groups.plugdev.name ];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "tss" config.users.groups.plugdev.name "input" "render" "video" ];
     packages = with pkgs; [];
     hashedPassword = "$y$j9T$PoIVXXZUTD0aNXvUtlmyK/$VJH7ZxK7V9Caq99dpvrjPhJY/nKrjrzBpHZYSdBWu53";
     # hashedPasswordFile = config.sops.secrets.trent-password.path;
